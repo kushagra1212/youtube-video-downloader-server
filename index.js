13e0dabdcccb72fa9acfd4da8a1733ebb8b37a15
unknown <kushagrarathore002@gmail.com>
2023-03-07 16:46:19 +0530

    make it better

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express=require('express');
-const app=express();
-const bodyparser=require('body-parser')
-const cors=require('cors')
-app.use(cors())
-const PORT=process.env.PORT||5000;
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
-const homerouter=require('./routers/homerouter')
+const homerouter = require('./routers/homerouter');
 
-app.use(express.json())
-app.use(bodyparser.json())
+app.use(express.json());
+app.use(bodyparser.json());
 
-app.use(bodyparser.urlencoded({
-    extended: true
-  }));
-app.use('/',homerouter)
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+app.use('/', homerouter);
 
-app.listen(PORT,()=>{
-    console.log("server is up running")
-})
+app.listen(PORT, () => {
+  console.log('server is up running');
+});
--- routers/homerouter.js
+++ routers/homerouter.js
@@ -1,68 +1,46 @@
-const express=require('express');
-const router=express.Router();
-const ytdl=require('ytdl-core');
-const fs=require('fs');
-const ytsr=require('ytsr')
-
+const express = require('express');
+const router = express.Router();
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const ytsr = require('ytsr');
 
 let body;
 
-router.get('/',(req,res)=>{
-    res.send("home")
-})
+router.get('/', (req, res) => {
+  res.send('home');
+});
 
-router.post('/download',(req,res)=>{
-     body=req.body.body;
-     
-     res.json({message:"Downloading video"})
-         
-    
-})
-router.get('/download2', async(req,res)=>{
-    const  {videoquality,videourl}=body;
-    console.log(videourl)
- 
-    const videoid= ytdl.getVideoID(videourl);
+router.post('/download', (req, res) => {
+  body = req.body.body;
 
-        const info= await ytdl.getInfo(videoid);
+  res.json({ message: 'Downloading video' });
+});
+router.get('/download2', async (req, res) => {
+  const { videoquality, videourl } = body;
+  console.log(videourl);
 
+  const videoid = ytdl.getVideoID(videourl);
 
+  const info = await ytdl.getInfo(videoid);
 
- 
-    if(info)
-    {
-        
-       
+  if (info) {
+    const format = ytdl.chooseFormat(info.formats, { quality: videoquality });
 
-        const format= ytdl.chooseFormat(info.formats,{quality:videoquality});
-  
-
-  
-   
-   
     res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
- res.setHeader('Content-Length',format.contentLength);
+    res.setHeader('Content-Length', format.contentLength);
 
+    ytdl(videourl, { format: 'mp4' }).pipe(res);
+  }
+});
 
-    ytdl(videourl,{format:'mp4'}).pipe(res);
-    
-    
-    }
-  
-})
+router.get('/search/:item/:limit', async (req, res) => {
+  const x = req.params.item;
 
-router.get('/search/:item/:limit',async(req,res)=>{
-   const x=req.params.item;
-   
-    try{
-        const result=await ytsr(x,{limit:parseInt(req.params.limit)})
-        res.json(result)
-    }catch(err){
-console.log(err)
-    }
-  
-    
-
-
-})
-module.exports=router;
+  try {
+    const result = await ytsr(x, { limit: parseInt(req.params.limit) });
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
+module.exports = router;
